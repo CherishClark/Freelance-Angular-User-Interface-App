a0fccbe880401882b531b133def94f08badb1a2c
CherishClark <cherish.g.clark@gmail.com>
2018-02-02 20:19:20 -0700

    creating form elements for proposals

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }from './app.component';
 import { HomepageComponent }from './homepage/homepage.component';
@@ -14,6 +15,7 @@ import { AppRoutingModule }from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
