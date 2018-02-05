0fc6498bf2259ff747ad666445c8dfc2f1d10a35
CherishClark <cherish.g.clark@gmail.com>
2018-02-05 14:26:30 -0700

    connected proposal microservice

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list.component.css']
+  styleUrls: ['proposal-list.component.css'],
+  providers: [ ProposalService ]
 })
+export class ProposalListComponent implements OnInit {
+  proposals: Proposal[];
+  errorMessage: string;
+  mode = "Observable";
 
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal (99, 'Abc Company', 'http://portfolio.jordanhdugens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
-  proposalTwo: Proposal = new Proposal (15, 'Blah Company', 'http://portfolio.jordanhdugens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
-  proposalThree: Proposal = new Proposal (20, 'Foo Company', 'http://portfolio.jordanhdugens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+  constructor(
+    private proposalService: ProposalService
+  ) {}
 
-  proposals: Proposal[] = [this.proposalOne, this.proposalTwo, this.proposalThree] 
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
 
-
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
