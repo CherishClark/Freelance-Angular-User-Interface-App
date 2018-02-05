739f0db64a0878a04796da1999d8f45e20469fb4
CherishClark <cherish.g.clark@gmail.com>
2018-02-05 11:24:02 -0700

    connected rails API/documentService to populate docs data from rails api

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent }from './app.component';
-import { HomepageComponent }from './homepage/homepage.component';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
 
-import { AppRoutingModule }from './app-routing.module';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpModule
   ],
   declarations: [
     AppComponent,
@@ -27,7 +28,7 @@ import { AppRoutingModule }from './app-routing.module';
     DocumentsComponent,
     ProposalListComponent,
     ProposalNewComponent,
-    ProposalShowComponent,
+    ProposalShowComponent
   ],
   providers: [
     DocumentService
@@ -36,5 +37,4 @@ import { AppRoutingModule }from './app-routing.module';
     AppComponent
   ]
 })
-
 export class AppModule {}
--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls:['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-
-export class DocumentsComponent{
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: "aasdfasdf",
-      file_url: 'http://google.com',
-      updated_at: '2/2',
-      image_url:'https://static.pexels.com/photos/461064/pexels-photo-461064.jpeg'
-    },
-    {
-      title: "My 2nd Doc",
-      description: "aasdfasdf",
-      file_url: 'http://google.com',
-      updated_at: '2/2',
-      image_url:'https://static.pexels.com/photos/461064/pexels-photo-461064.jpeg'
-    },
-    {
-      title: "My last Doc",
-      description: "aasdfasdf",
-      file_url: 'http://google.com',
-      updated_at: '2/2',
-      image_url:'https://static.pexels.com/photos/461064/pexels-photo-461064.jpeg'
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
